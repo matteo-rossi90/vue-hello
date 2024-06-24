b2136371d477c9bd6c0c84f257e71e74c736630d
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-24 14:49:29 +0200

    feat: add "Hello Vue!" in DOM with data()

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 /*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.*/
 
+//richiamare l'API di Vue CreateApp
+
+const { createApp } = Vue;
+
+//creare un messaggio da stampre in DOM
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}).mount('#text')
